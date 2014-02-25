9f13423e666d86cf52f2c6bd70b92aa7bc04303c
Skjoenberg <xtf221@alumni.ku.dk>
2014-02-25 13:11:54 +0100

    Added unfinished Move class

---
+++ Move.java
@@ -0,0 +1,12 @@
+public class Move {
+
+    public void move(List moveList){
+        for(String move : moveList){
+            switch (move) {
+            case u: Controls.up(); break;
+
+            }
+        }
+    }
+
+}
