68579f4cf67b58f149f633b862ffbe94b5243284
Skjoenberg <xtf221@alumni.ku.dk>
2014-02-25 21:48:44 +0100

    added unfinished map class

---
+++ Map.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList();
+
+public class Map {
+
+    private final char rock = '#';
+    private final char grass = 'm';
+    private final char road = ' ';
+    private final char water ='w';
+
+    private ArrayList<ArrayList<char>> map;
+
+    public Map(){
+        map = New ArrayList<ArrayList<Char>>();
+
+    }
+
+}
