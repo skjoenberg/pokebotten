9f7977ca239dc7547ac6501bd4ecfc915235bc2b
Sebastian S. A <sebastian@skjoenberg.com>
2014-02-25 23:56:40 +0100

    finished the Map class

--- Map.java
+++ Map.java
@@ -1,17 +1,54 @@
-import java.util.ArrayList();
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Map {
+    private Scanner in;
 
     private final char rock = '#';
-    private final char grass = 'm';
+    private final char grass = 'g';
     private final char road = ' ';
     private final char water ='w';
 
-    private ArrayList<ArrayList<char>> map;
+    private char[][] map;
+    private int width;
+    private int height;
 
     public Map(){
-        map = New ArrayList<ArrayList<Char>>();
-
+        map = null;
     }
 
+    public void readMap(String fileName){
+        try{
+            File file = new File(fileName);
+            in = new Scanner(file);
+            width = in.nextInt();
+            height = in.nextInt();
+
+            for (int i = 0; height > i; i++){
+                char[] line = in.nextLine().toCharArray();
+                for (int j = 0; width > j; j++){
+                    map[j][i] = line[j];
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isRock(int x, int y){
+        return (map[x][y] == rock);
+    }
+
+    public boolean isWater(int x, int y){
+        return (map[x][y] == water);
+    }
+
+    public boolean isGrass(int x, int y){
+        return (map[x][y] == grass);
+    }
+
+    public boolean isRoad(int x, int y){
+        return (map[x][y] == road);
+    }
 }
