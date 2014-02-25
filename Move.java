4e1e9cd03c056c9cc2bb46f48b00e1b0c9808872
Skjoenberg <xtf221@alumni.ku.dk>
2014-02-25 15:37:29 +0100

    Finished the move method.

--- Move.java
+++ Move.java
@@ -1,10 +1,16 @@
 public class Move {
 
+    // Parsing a list of moves
     public void move(List moveList){
         for(String move : moveList){
             switch (move) {
             case u: Controls.up(); break;
-
+            case d: Controls.down(); break;
+            case l: Controls.left(); break;
+            case r: Controls.right(); break;
+            case a: Controls.a(); break;
+            case b: Controls.b(); break;
+            default: System.out.println("Illegal Move!"); break;
             }
         }
     }
