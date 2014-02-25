657a8b7a19c698af093b59bcc20f8af3ef9bd307
Sebastian S. A <sebastian@skjoenberg.com>
2014-02-25 23:57:14 +0100

    fixed some syntax errors

--- Move.java
+++ Move.java
@@ -1,15 +1,17 @@
+import java.util.ArrayList;
+
 public class Move {
 
     // Parsing a list of moves
-    public void move(List moveList){
+    public void move(ArrayList<String> moveList){
         for(String move : moveList){
             switch (move) {
-            case u: Controls.up(); break;
-            case d: Controls.down(); break;
-            case l: Controls.left(); break;
-            case r: Controls.right(); break;
-            case a: Controls.a(); break;
-            case b: Controls.b(); break;
+            case "u": Controls.up(); break;
+            case "d": Controls.down(); break;
+            case "l": Controls.left(); break;
+            case "r": Controls.right(); break;
+            case "a": Controls.a(); break;
+            case "b": Controls.b(); break;
             default: System.out.println("Illegal Move!"); break;
             }
         }
