1d7aaf78475b406547e1c92c27bf60a6af5bc125
Sebastian S. A <sebastian@skjoenberg.com>
2014-02-26 01:08:48 +0100

    added loading function for pokedex and maplist

--- Init.java
+++ Init.java
@@ -1,8 +1,34 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Init {
 
     public static void main(String[] args) {
-        Pokedex pokedex = new Pokedex();
+        Pokedex pokedex;
+        MapList mapList;
 
-        pokedex.addPokemon();
+        ObjectInputStream objIn;
+
+        //Loading poke.dex file
+        try{
+            objIn = new ObjectInputStream(new FileInputStream("Poke.dex"));
+            pokedex = (Pokedex) objIn.readObject();
+            objIn.close();
+            System.out.println("Pokedex loaded successfully");
+        } catch (IOException | ClassNotFoundException e) {
+            pokedex = new Pokedex();
+            System.out.println("Pokedex did not load successfully");
+        }
+
+        //Loading list.maps file
+        try{
+            objIn = new ObjectInputStream(new FileInputStream("list.maps"));
+            mapList = (MapList) objIn.readObject();
+            objIn.close();
+            System.out.println("Maplist loaded successfully");
+        } catch (IOException | ClassNotFoundException e) {
+            mapList = new MapList();
+            System.out.println("Maplist did not load successfully");
+        }
     }
 }
